25362b8702f519c5fadd4d1ad01135fcd06fc7b7
Md Najeebur Rahman <iamnajeebjung@gmail.com>
2025-12-22 18:33:22 +0530

    .

--- Crypto-Wallet/src/components/Footer.jsx
+++ Crypto-Wallet/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='relative w-full  h-full'>
-            <div className='  absolute border bottom-7 lg:bottom-5 flex justify-between w-full px-10'>
+            <div className='  absolute border bottom-3 lg:bottom-5 flex justify-between w-full px-10'>
 
                 <div className='bg-[#222] rounded-lg text-white w-10 h-10 p-2 cursor-pointer  hover:bg-[#333333] transition-all duration-300'>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="rgba(248,245,245,0.92)"><path d="M6 19H18V9.15745L12 3.7029L6 9.15745V19ZM19 21H5C4.44772 21 4 20.5523 4 20V11L1 11L11.3273 1.6115C11.7087 1.26475 12.2913 1.26475 12.6727 1.6115L23 11L20 11V20C20 20.5523 19.5523 21 19 21ZM8 9.99998C11.866 9.99998 15 13.134 15 17H13C13 14.2386 10.7614 12 8 12V9.99998ZM8 14C9.65685 14 11 15.3431 11 17H8V14Z"></path></svg>
